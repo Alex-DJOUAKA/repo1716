20bd9af3215a0837ac6143eab47c114089168884
Alex DJOUAKA TEMGOUA <alex.djouaka@yahoo.fr>
2021-11-23 15:39:03 +0100

    mysql

---
+++ 00-cours/Java Scrip/site-formulaire/assets/js/js fichier.js
@@ -0,0 +1,10 @@
+
+var nom = document.getElementById("nom").value;
+console.log(typeof(nom.value));
+
+function afficher() {
+    var saisie =document.getElementById ("textel"). value;
+    document.getElementById("texte2").value=saisie:
+    }
+    
+    
